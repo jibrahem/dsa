a1792ed90aaa3f4079a8ad15e680451ebc7ed286
JehanIbrahem <jehan.ibrahem25@gmail.com>
2024-02-14 11:11:50 -0800

    added the solution

--- leet-code/add_two_numbers.js
+++ leet-code/add_two_numbers.js
@@ -13,5 +13,26 @@ You may assume the two numbers do not contain any leading zero, except the numbe
  * @return {ListNode}
  */
 var addTwoNumbers = function (l1, l2) {
+    let dummyHead = new ListNode(0); // Dummy head to simplify the code
+    let current = dummyHead;
+    let carry = 0;
 
+    while (l1 || l2) {
+        let x = l1 ? l1.val : 0;
+        let y = l2 ? l2.val : 0;
+        let sum = carry + x + y;
+
+        carry = Math.floor(sum / 10);
+        current.next = new ListNode(sum % 10);
+        current = current.next;
+
+        if (l1) l1 = l1.next;
+        if (l2) l2 = l2.next;
+    }
+
+    if (carry > 0) {
+        current.next = new ListNode(carry);
+    }
+
+    return dummyHead.next;
 };
