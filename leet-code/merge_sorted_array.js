aac073394cbd50b7d024e0c1463d3f761b2c0978
JehanIbrahem <jehan.ibrahem25@gmail.com>
2024-02-24 18:54:47 -0800

    added the solution

--- leet-code/merge_sorted_array.js
+++ leet-code/merge_sorted_array.js
@@ -14,5 +14,22 @@ The final sorted array should not be returned by the function, but instead be st
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function (nums1, m, nums2, n) {
-
+    if (n === 0) return;
+    let len1 = nums1.length;
+    let end_idx = len1 - 1;
+    while (n > 0 && m > 0) {
+        if (nums2[n - 1] >= nums1[m - 1]) {
+            nums1[end_idx] = nums2[n - 1];
+            n--;
+        } else {
+            nums1[end_idx] = nums1[m - 1];
+            m--;
+        }
+        end_idx--;
+    }
+    while (n > 0) {
+        nums1[end_idx] = nums2[n - 1];
+        n--;
+        end_idx--;
+    }
 };
