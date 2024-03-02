9f3590db9bf00fb2079cc775119175c6029ac4b3
JehanIbrahem <jehan.ibrahem25@gmail.com>
2024-03-02 13:17:03 -0800

    added the solution

--- leet-code/majority_element.js
+++ leet-code/majority_element.js
@@ -7,7 +7,17 @@ The majority element is the element that appears more than ⌊n / 2⌋ times.You
  * @param {number[]} nums
  * @return {number}
  */
-var majorityElement = function (nums) {
 
-    
+
+var majorityElement = function (nums) {
+    let candidate, count = 0
+    for (let num of nums) {
+        if (count === 0) {
+            candidate = num
+        }
+
+        count += (num === candidate) ? 1 : -1
+    }
+
+    return candidate;
 };
