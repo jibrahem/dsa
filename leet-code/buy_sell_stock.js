3af105a6861153dc6782758802fa3fa2ed8e72c4
JehanIbrahem <jehan.ibrahem25@gmail.com>
2024-03-02 14:09:06 -0800

    added the solution

--- leet-code/buy_sell_stock.js
+++ leet-code/buy_sell_stock.js
@@ -11,4 +11,20 @@ Return the maximum profit you can achieve from this transaction.If you cannot ac
  */
 var maxProfit = function (prices) {
 
+    function maximumProfit(prices) {
+        const n = prices.length;
+        let profit = 0;
+        let minPrice = prices[0];
+
+        for (let i = 1; i < n; ++i) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else {
+                profit = Math.max(profit, prices[i] - minPrice);
+            }
+        }
+
+        return profit;
+    }
+
 };
