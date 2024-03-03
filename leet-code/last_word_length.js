a56725653fa82c26f2426cff07b73c0e9841db8c
JehanIbrahem <jehan.ibrahem25@gmail.com>
2024-03-02 17:39:29 -0800

    added another problem

---
+++ leet-code/last_word_length.js
@@ -0,0 +1,13 @@
+/* Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal
+substring
+ consisting of non - space characters only.
+*/
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+
+};
