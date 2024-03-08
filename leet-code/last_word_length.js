33aaa9bcd973e83438ce732ffb7dc71bdc8a5d54
JehanIbrahem <jehan.ibrahem25@gmail.com>
2024-03-08 12:55:44 -0800

    added word pattern problem

--- leet-code/last_word_length.js
+++ leet-code/last_word_length.js
@@ -9,5 +9,6 @@ substring
  * @return {number}
  */
 var lengthOfLastWord = function (s) {
+    return s.trimEnd().split(' ').pop().length
 
 };
---
+++ leet-code/word_pattern.js
@@ -0,0 +1,13 @@
+/* Given a pattern and a string s, find if s follows the same pattern.
+
+Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
+*/
+
+/**
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+var wordPattern = function (pattern, s) {
+
+};
