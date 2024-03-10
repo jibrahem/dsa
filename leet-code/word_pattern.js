6e3b710ebe5770ea1aa6d1e03f4fa5398bcc74a0
JehanIbrahem <jehan.ibrahem25@gmail.com>
2024-03-09 19:28:17 -0800

    added solution

--- leet-code/word_pattern.js
+++ leet-code/word_pattern.js
@@ -9,5 +9,14 @@ Here follow means a full match, such that there is a bijection between a letter 
  * @return {boolean}
  */
 var wordPattern = function (pattern, s) {
-
+    const words = s.split(' ');
+    if (pattern.length !== words.length) return false;
+    const obj = {};
+    for (let i = 0; i < words.length; i++) {
+        if ((obj[pattern[i]] && obj[pattern[i]] !== words[i])) {
+            return false;
+        }
+        obj[pattern[i]] = words[i];
+    }
+    return new Set(Object.values(obj)).size === Object.values(obj).length;
 };
