483f6a1d5263c4e92ff90691c48775c8df476d96
JehanIbrahem <jehan.ibrahem25@gmail.com>
2024-03-16 17:26:21 -0700

    pasted the solution

--- SQL/prac6.sql
+++ SQL/prac6.sql
@@ -8,3 +8,10 @@ Return the result table in any order.
 
 The result format is in the following example.
 */
+
+select a1.machine_id, round(avg(a2.timestamp-a1.timestamp), 3) as processing_time
+from Activity a1
+join Activity a2
+on a1.machine_id=a2.machine_id and a1.process_id=a2.process_id
+and a1.activity_type='start' and a2.activity_type='end'
+group by a1.machine_id
