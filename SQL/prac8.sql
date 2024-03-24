a823e8b6257092b27d0aed0baa51917ad4c7f7b9
JehanIbrahem <jehan.ibrahem25@gmail.com>
2024-03-23 20:57:07 -0700

    added another sql question

---
+++ SQL/prac8.sql
@@ -0,0 +1,7 @@
+/*
+Write a solution to find the average selling price for each product. average_price should be rounded to 2 decimal places.
+
+Return the result table in any order.
+
+The result format is in the following example.
+*/
